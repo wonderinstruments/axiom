180aee4600d3b86d0d5ba2b6c3c0ac1730871489
Edmund Mills <edmund.martin.mills@gmail.com>
2025-10-19 19:54:14 +0000

    feat: debug admin module

--- configurable_modules/admin.nix
+++ configurable_modules/admin.nix
@@ -8,8 +8,12 @@ let
   cfg = config.admin;
 in
 {
-  options.webBrowser = {
-    enable = lib.mkEnableOption "Enable firefox browser";
+  options = {
+    admin = {
+      webBrowser = {
+        enable = lib.mkEnableOption "Enable firefox browser";
+      };
+    };
   };
 
   config = lib.mkIf cfg.webBrowser.enable {
