8ddd994f1336e751bcf2da94361a01e10e7abdd1
Edmund Mills <edmund.martin.mills@gmail.com>
2025-10-22 16:02:15 +0000

    feat: set up matrix screensaver

--- modules/screen-locker.nix
+++ modules/screen-locker.nix
@@ -1,32 +1,31 @@
 { config, pkgs, ... }:
-let 
+let
+  pidfile = "$HOME/.cache/cmatrix.pid";
+
   cmatrixSaver = pkgs.writeShellScriptBin "cmatrix-saver" ''
     #!/usr/bin/env bash
-    set -euo pipefall
-    exec ${pkgs.kitty}/bin/kitty -fullscreen -fa "Monospace" -fs 14 \
+    set -euo pipefail
+    exec ${pkgs.xterm}/bin/xterm -fullscreen -fa "Monospace" -fs 14 \
          -e ${pkgs.cmatrix}/bin/cmatrix -a -b
   '';
-  cmatrixIdle = pkgs.writeShellScriptBin "cmatrix-idle" ''
-   #!/usr/bin/env bash
-   set -euo pipefall
-   pidfile="${XDG_RUNTIME_DIR:-run/user/$UID}/cmatrix.pid"
-   timeout_cmd='${cmatrixSaver}/bin/cmatrix-saver & echo $! > '"$pidfile"
-   resume_cmd='[ -f '"$pidfile"' ]  && kill "$(cat '"$pidfile"')" 2>/dev/null || true; rm -f '"pidfile"
-   exec ${pkgs.swayidle}/bin/swayidle -w \
-     timeout 30 "$timeout_cmd" \
-     resume "$resume_cmd"
-  '';
+
+  timeoutCmd = ''${cmatrixSaver}/bin/cmatrix-saver & echo $! > ${pidfile}'';
+  cancellerCmd = ''[ -f ${pidfile} ] && kill "$(cat ${pidfile})" 2>/dev/null || true; rm -f ${pidfile}'';
 in
 {
-  home.packages = with pkgs; [ cmatrix kitty swayidle cmatrixSaver swayidleScript ]
-  systemd.user.services.cmatrix-swayidle = {
-    Unit = { Description = "cmatrix screensaver via swayidle"; };
-    Service = {
-      ExecStart = "${swayidleScript}/bin/cmatrix-swayidle";
-      Restart = "always";
-      Environment = "XDG_CURRENT_DESKTOP=sway";
-    };
-    Install = { WantedBy = [ "graphical-session.target" ]; };
+  home.packages = with pkgs; [
+    cmatrix
+    cmatrixSaver
+  ];
+
+  services.xidlehook = {
+    enable = true;
+    timers = [
+      {
+        delay = 120;
+        command = timeoutCmd;
+        canceller = cancellerCmd;
+      }
+    ];
   };
-  systemd.user.startServices = "sd-switch"
 }
