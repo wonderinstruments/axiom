410331ed8835573b79adb0829d7ecc95cc4ef7fb
Edmund Mills <edmund.martin.mills@gmail.com>
2025-10-22 14:09:20 +0000

    feat: configure i3 bar colors

--- modules/i3.nix
+++ modules/i3.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   xsession.windowManager.i3 = {
     enable = true;
@@ -7,6 +7,48 @@
         {
           command = "i3-msg 'workspace 1; exec kitty'";
           always = false;
+        }
+      ];
+      bars = [
+        {
+          id = "bottom";
+          mode = "dock";
+          position = "bottom";
+          fonts = {
+            names = [ "pango:Atkinson Hyperlegible Next" ];
+            style = "Bold";
+            size = 14.0;
+          };
+          colors = {
+            background = "#${config.lib.stylix.colors.base00}";
+            statusline = "#${config.lib.stylix.colors.base05}";
+            separator = "#${config.lib.stylix.colors.base03}";
+            focusedWorkspace = {
+              border = "#${config.lib.stylix.colors.base0D}";
+              background = "#${config.lib.stylix.colors.base0D}";
+              text = "#${config.lib.stylix.colors.base00}";
+            };
+            activeWorkspace = {
+              border = "#${config.lib.stylix.colors.base03}";
+              background = "#${config.lib.stylix.colors.base03}";
+              text = "#${config.lib.stylix.colors.base05}";
+            };
+            inactiveWorkspace = {
+              border = "#${config.lib.stylix.colors.base01}";
+              background = "#${config.lib.stylix.colors.base01}";
+              text = "#${config.lib.stylix.colors.base05}";
+            };
+            urgentWorkspace = {
+              border = "#${config.lib.stylix.colors.base08}";
+              background = "#${config.lib.stylix.colors.base08}";
+              text = "#${config.lib.stylix.colors.base00}";
+            };
+            bindingMode = {
+              border = "#${config.lib.stylix.colors.base0A}";
+              background = "#${config.lib.stylix.colors.base0A}";
+              text = "#${config.lib.stylix.colors.base00}";
+            };
+          };
         }
       ];
     };
