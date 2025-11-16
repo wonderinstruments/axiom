bfe4cb9698bafe1df3c169c3c7c63423f51cf92b
Edmund Mills <edmund.martin.mills@gmail.com>
2025-11-16 15:21:42 +0000

    feat: add admin apps to rofi

--- home.nix
+++ home.nix
@@ -4,4 +4,5 @@
 {
   home.stateVersion = "25.05";
   programs.bat.enable = true;
+  nixpkgs.config.allowUnfree = true;
 }
--- modules/admin.nix
+++ modules/admin.nix
@@ -94,9 +94,57 @@ in
   config = lib.mkMerge [
     (lib.mkIf cfg.webBrowser.enable {
       programs.firefox.enable = true;
+      xdg.desktopEntries.firefox = {
+        name = "Firefox";
+        comment = "Web Browser";
+        exec = "firefox";
+        icon = "firefox";
+        categories = [
+          "Network"
+          "WebBrowser"
+        ];
+        terminal = false;
+        startupNotify = true;
+      };
+      xdg.dataFile."applications/rofi-firefox.desktop".text = ''
+        [Desktop Entry]
+        Version=1.0
+        Type=Application
+        Name=firefox
+        Comment=Web Browser
+        Exec=firefox
+        Icon=firefox
+        Categories=Network;WebBrowser;RofiCustom;
+        Terminal=false
+        StartupNotify=true
+      '';
     })
     (lib.mkIf cfg.chromium.enable {
       programs.chromium.enable = true;
+      xdg.desktopEntries.chromium = {
+        name = "Chromium";
+        comment = "Web Browser";
+        exec = "chromium";
+        icon = "chromium";
+        categories = [
+          "Network"
+          "WebBrowser"
+        ];
+        terminal = false;
+        startupNotify = true;
+      };
+      xdg.dataFile."applications/rofi-chromium.desktop".text = ''
+        [Desktop Entry]
+        Version=1.0
+        Type=Application
+        Name=chromium
+        Comment=Web Browser
+        Exec=chromium
+        Icon=chromium
+        Categories=Network;WebBrowser;RofiCustom;
+        Terminal=false
+        StartupNotify=true
+      '';
     })
     {
       # Install enabled admin applications
