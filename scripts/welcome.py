befd05292276b5a6de26890136bedd5e9c4f8078
Edmund Mills <edmund.martin.mills@gmail.com>
2025-11-19 14:30:47 +0000

    update welcome

--- scripts/welcome.py
+++ scripts/welcome.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
 import pyfiglet
+from colorama import Fore, Style
 
-# Print AXIOM in figlet
-print(pyfiglet.figlet_format("AXIOM", font="isometric3"))
+# Print AXIOM in figlet with color
+axiom_text = pyfiglet.figlet_format("AXIOM", font="roman")
+print(Fore.CYAN + axiom_text + Style.RESET_ALL)
 
 # Print helpful message
-print("For a quick reference guide, read the welcome file by typing: `bat ~/docs/WELCOME.md`, then press enter.\n")
+print(Fore.GREEN + "For a quick reference guide, read the welcome file by typing: " +
+      Style.RESET_ALL + Fore.MAGENTA + "bat ~/docs/WELCOME.md" +
+      Style.RESET_ALL + Fore.GREEN + ", then press enter." +
+      Style.RESET_ALL + "\n")
