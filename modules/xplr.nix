8b96502904c7e01beb55e48268649fad474593df
Edmund Mills <edmund.martin.mills@gmail.com>
2025-11-25 06:18:18 +0000

    fix xplr config

--- modules/xplr.nix
+++ modules/xplr.nix
@@ -1,40 +1,28 @@
 { pkgs, ... }:
-let
-  nukePlugin = pkgs.fetchFromGitHub {
-    owner = "Junker";
-    repo = "nuke.xplr";
-    rev = "main";
-    sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # Update this after first build
-  };
-in
 {
-  # Link nuke plugin
-  xdg.configFile."xplr/plugins/nuke".source = nukePlugin;
-
-  # Generate init.lua with configuration
-  xdg.configFile."xplr/init.lua".text = ''
-    -- Add plugin path
-    local home = os.getenv("HOME")
-    package.path = home
-      .. "/.config/xplr/plugins/?/src/init.lua;"
-      .. home
-      .. "/.config/xplr/plugins/?.lua;"
-      .. package.path
-
-    -- Setup nuke plugin with open behavior only
-    require("nuke").setup{
-      open = {
-        run_executables = false,
-        custom = {
-          {mime_regex = ".*", command = "xdg-open {}"}
+  programs.xplr = {
+    enable = true;
+    plugins = {
+      nuke = pkgs.fetchFromGitHub {
+        owner = "Junker";
+        repo = "nuke.xplr";
+        rev = "main";
+        sha256 = "sha256-k/yre9SYNPYBM2W1DPpL6Ypt3w3EMO9dznHwa+fw/n0=";
+      };
+    };
+    extraConfig = ''
+      require("nuke").setup{
+        open = {
+          run_executables = false,
+          custom = {
+            {mime_regex = ".*", command = "xdg-open {}"}
+          }
         }
       }
-    }
 
-    -- Key bindings
-    local key = xplr.config.modes.builtin.default.key_bindings.on_key
-
-    -- Enter key opens files
-    key["enter"] = xplr.config.modes.custom.nuke.key_bindings.on_key.o
-  '';
+      -- Enter key opens files
+      local key = xplr.config.modes.builtin.default.key_bindings.on_key
+      key["enter"] = xplr.config.modes.custom.nuke.key_bindings.on_key.o
+    '';
+  };
 }
