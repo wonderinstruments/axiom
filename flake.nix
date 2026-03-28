e0b70033143294a2f8598311a48e0b0f86e99e37
Edmund Mills <edmund.martin.mills@gmail.com>
2026-03-28 11:53:40 -0500

    update brightworker

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
       url = "github:unhappychoice/gittype";
     };
     brightworker = {
-      url = "github:brightworks/brightworker-release/v0.0.1";
+      url = "github:brightworks/brightworker-release/v0.0.3";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
   };
